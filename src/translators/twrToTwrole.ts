57d40fd52628153a0e6bde5e1882d1a5d6e99bfb
Dipsy Wong <ycwongal@connect.ust.hk>
2021-03-06 17:09:55 +0800

    fix missing head

--- src/translators/twrToTwrole.ts
+++ src/translators/twrToTwrole.ts
@@ -1,6 +1,25 @@
 import { Twr, Twrole } from '../type'
 
 export const twrToTwrole = (twr: Twr): Twrole => {
+  const deco = twr.decos.map(d => ({
+      ...d,
+      c: d.code.replace('_head_', 'head'),
+      sx: d.sx/2,
+      sy: d.sy/2,
+      r: d.r * Math.PI / 180
+    }
+  ))
+  if(twr.decos.findIndex(({code}) => code === '_head_') === -1) {
+    deco.unshift({
+      code: 'head',
+      c: 'head',
+      x: 0,
+      y: 0,
+      sx: 1,
+      sy: 1,
+      r: 0
+    })
+  }
   return {
     data: {
       dr: 13, // any camp male
@@ -9,14 +28,7 @@ export const twrToTwrole = (twr: Twr): Twrole => {
           f: 11,
           s: 1
         },
-        deco: twr.decos.map(d => ({
-            ...d,
-            c: d.code.replace('_head_', 'head'),
-            sx: d.sx/2,
-            sy: d.sy/2,
-            r: d.r * Math.PI / 180
-          }
-        )),
+        deco,
         foot: { f: twr.foot, s: 1 },
         hand: { f: twr.hand, s: 1 },
         head: { f: twr.head, s: 1 }
